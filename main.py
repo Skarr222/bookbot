455f5f63b69d8f06fab9c0e626b0cf27ee8152e5
Skarr222 <oskargomza54@gmail.com>
2025-03-03 16:58:03 +0100

    reading book from file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text():
+    path_to_file = "books/frankenstein.txt"
+    file_content = ""
+    with open(path_to_file) as f:
+        file_content = f.read()
+    return file_content
+    
+def main():
+    book = get_book_text()
+    print(book)
+    
+main()
