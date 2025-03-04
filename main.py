6f96d3e0f27d3d4e715137a2dfe702c4fca3d97f
Skarr222 <oskargomza54@gmail.com>
2025-03-04 15:09:38 +0100

    complete assginment from boot.dev

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-from stats import get_book_length, get_count_of_characters
-def get_book_text():
-    path_to_file = "books/frankenstein.txt"
+from stats import get_book_length, get_count_of_characters, sort_dictionary
+import sys 
+
+def get_book_text(path_to_book):
     file_content = ""
-    with open(path_to_file) as f:
+    with open(path_to_book) as f:
         file_content = f.read()
     return file_content
     
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)     
     try:
-        book = get_book_text()
+        path_to_file = sys.argv[1]
+        print(path_to_file)
+        book = get_book_text(path_to_file)
         book_length = get_book_length(book)
         character_dictionary = get_count_of_characters(book)
-        print(f"{book_length} words found in the document")
-        print(character_dictionary)
-        
+        sorted_dictionary = sort_dictionary(character_dictionary)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path_to_file}...")
+        print("----------- Word Count ----------")
+        print(f"Found {book_length} total words")
+        print("--------- Character Count -------")
+        for dictionary  in sorted_dictionary:
+            character = dictionary["character"]
+            count = dictionary["count"]
+            print(f"{character}: {count}")
+        print("============= END ===============")
     except Exception as error:
         print(error)
     
-    
-main()
+if __name__ == "__main__":
+    main()
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+from curses.ascii import isalpha
+
+
 def get_book_length(book):
     book_splited = book.split()
     num_of_words = len(book_splited)
@@ -15,4 +18,12 @@ def get_count_of_characters (book):
                 book_dictonary[char] = 1
         
     return book_dictonary    
-    
+
+def sort_dictionary(char_dic):
+    sorted_dictionary = {}
+    sorted_list = sorted(
+        [{"character": char, "count": count} for char, count in char_dic.items() if char.isalpha()],
+        key=lambda x: x["count"],
+        reverse=True
+    )
+    return sorted_list
